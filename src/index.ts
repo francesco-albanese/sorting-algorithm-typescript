6dd04907c584dc8e2dd54d788b65df772a7ddcd8
Francesco Albanese <francescoalbanese@Francescos-MacBook-Pro.local>
2019-11-04 23:48:04 +0000

    implement first version of sorting algorithm  This will swap the position of two elements if the leftHand one is greater than the rightHand one

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log('Hi there!')
+class Sorter {
+  constructor(public collection: number[]) {}
+  
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      /**
+       * j < length - i - 1
+       * the - i is going to account for the fact
+       * that once we do a full iteration though
+       * the array, the rightmost element will be
+       * in the correct location so we don't have
+       * to sort it again in the future.
+       */
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          //shift element positions
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([-1, 10, 5, 0, 3]);
+sorter.sort();
+console.log('sorter.collection', sorter.collection);
