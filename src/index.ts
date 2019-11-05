9efe4dcc75656f91050b29f336eaeb40c37d1939
Francesco Albanese <francescoalbanese@Francescos-MacBook-Pro.local>
2019-11-05 22:39:23 +0000

    Create interface Sortable

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ import { Sorter } from './Sorter';
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbersCollection.data);
