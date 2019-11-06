80a8cf1bd311d9262248c4db48a1975cddc4ac69
Francesco Albanese <francescoalbanese@Francescos-MacBook-Pro.local>
2019-11-06 20:07:38 +0000

    Add code to sort a linked list

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
+import { LinkedList } from './LinkedList';
 import { CharactersCollection } from './CharactersCollection';
 import { NumbersCollection } from './NumbersCollection';
-import { Sorter } from './Sorter';
+import { Sorter } from './Sorter'; 
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const charactersCollection = new CharactersCollection('XaayabB');
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
 const numberSorter = new Sorter(numbersCollection);
 const stringSorter = new Sorter(charactersCollection);
+const linkedListSorter = new Sorter(linkedList);
+
 numberSorter.sort();
 stringSorter.sort();
+linkedListSorter.sort();
+
 console.log(numbersCollection.data);
 console.log(charactersCollection.data);
+linkedList.print();
